77937bb3b33f3f005ec01b831c3ab200ca8c386f
CWS\vladyslav.ovsiychuk <vladyslav.ovsiychuk.cws@gmail.com>
2020-09-15 11:42:04 +0200

    first change into my_test

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -41,6 +41,7 @@ public class MainController {
         System.out.println("test");
 	    System.out.println("changes from github");
 	    System.out.println("add other changes from pc to new branch + 1 changes (ok I see it in github)");
+	    System.out.println("changes into my test)");
 
 
         if (filter != null && !filter.isEmpty()) {
