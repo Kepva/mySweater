3b829dbbca2e43e67859cc28df8f5cefbf8e3f4a
CWS\vladyslav.ovsiychuk <vladyslav.ovsiychuk.cws@gmail.com>
2020-09-15 11:52:55 +0200

    +1 my_test chanhre

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -41,7 +41,7 @@ public class MainController {
         System.out.println("test");
 	    System.out.println("changes from github");
 	    System.out.println("add other changes from pc to new branch + 1 changes (ok I see it in github)");
-	    System.out.println("changes into my test)");
+	    System.out.println("changes into my test + 1 other change)");
 
 
         if (filter != null && !filter.isEmpty()) {
